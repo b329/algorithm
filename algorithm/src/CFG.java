8b1dca042b6f9906a16ae9fac84f8ae77797b6ce
나영철 <4e65rv@feelway.com>
2021-03-22 00:47:57 +0900

    array

--- algorithm/src/CFG.java
+++ algorithm/src/CFG.java
@@ -1,70 +1,48 @@
-import javafx.util.Pair;
-import java.util.ArrayList;
-import java.util.*;
+// Java program to find
+// duplicates in the given array
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
-class GfG
+class FindDuplicatesArraysEfficientApproach
 {
     // Function returns the minimum number of swaps 
     // required to sort the array 
-    public static int distinct(int[] A)
-    {
-        int n = A.length;
-
-        ArrayList <Pair <Integer, Integer> > arrpos =
-                new ArrayList <Pair <Integer, Integer> > ();
-        for (int i = 0; i < n; i++)
-            arrpos.add(new Pair <Integer, Integer> (A[i], i));
-        arrpos.sort(new Comparator<Pair<Integer, Integer>>()
-        {
-            @Override
-            public int compare(Pair<Integer, Integer> o1,
-                               Pair<Integer, Integer> o2)
-            {
-                if (o1.getKey() > o2.getKey())
-                    return -1;
-                else if (o1.getKey().equals(o2.getKey()))
-                    return 0;
-
-                else
-                    return 1;
-            }
-        });
-
-        Boolean[] vis = new Boolean[n];
-        Arrays.fill(vis, false);
-        int answer = 0;
-
-        for (int i = 0; i < n; i++)
-        {
-            if (vis[i] || arrpos.get(i).getValue() == i)
-                continue;
-            int cSize = 0;
-            int j = i;
-            while (!vis[j])
-            {
-                vis[j] = true;
-
-                j = arrpos.get(j).getValue();
-                cSize++;
-            }
-
-            if(cSize > 0)
-            {
-                answer += (cSize - 1);
-            }
-        }
-        return answer;
-    }
-}
-
-// Driver class 
-class MinSwaps
-{
-    // Driver program to test the above function 
+    // Driver program
     public static void main(String[] args)
     {
-        int []a = {1, 5, 4, 3, 2};
-        GfG g = new GfG();
-        System.out.println(g.distinct(a));
+        int arr[] = {12, 11, 40, 12, 5, 6, 5, 12, 11};
+        int n = arr.length;
+        printDuplicates(arr, n);
+    }
+    // function to find and print duplicates
+    private static void printDuplicates(int[] arr, int n)
+    {
+        Map<Integer,Integer> map = new HashMap<>();
+        int count = 0;
+        boolean dup = false;
+        for(int i = 0; i < n; i++){
+            if(map.containsKey(arr[i])){
+                count = map.get(arr[i]);
+                map.put(arr[i], count + 1);
+            }
+            else{
+                map.put(arr[i], 1);
+            }
+        }
+
+        for(Entry<Integer,Integer> entry : map.entrySet())
+        {
+            // if frequency is more than 1
+            // print the element
+            if(entry.getValue() > 1){
+                System.out.print(entry.getKey()+ " ");
+                dup = true;
+            }
+        }
+        // no duplicates present
+        if(!dup){
+            System.out.println("-1");
+        }
     }
 } 
